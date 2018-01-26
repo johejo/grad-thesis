4d674e5b9919dbbaa847aa778d14a90d8b3e53df
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-01-26 21:31:44 +0900

    fix .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+ #!/bin/bash
+ 
  curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
  chmod +x dropbox_uploader.sh
  echo "OAUTH_ACCESS_TOKEN=($DROPBOX_TOKEN)"  >  ~/.dropbox_uploader
